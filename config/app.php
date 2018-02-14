d198630cedcd8e6cd17e2f7dca1d3dfebde93003
zhangyang <whoshizhangyang@hotmail.com>
2018-02-14 14:51:09 +0000

    Admin can manage users

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Frozennode\Administrator\AdministratorServiceProvider,
+        Frozennode\Administrator\AdministratorServiceProvider::class,
+
     ],
 
     /*
