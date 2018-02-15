52c7dad4cbe9a815ae5f62d666c93fa87f4f0958
zhangyang <whoshizhangyang@hotmail.com>
2018-02-15 08:22:12 +0000

    user can register using phone

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
     }
 
     protected $fillable = [
-        'name', 'email', 'password', 'introduction', 'avatar'
+        'name', 'phone', 'email', 'password', 'introduction', 'avatar',
     ];
 
     public function replies()
