afe26cf0deb0cb8f8656c4f6d5d2cd364a08693f
zhangyang <whoshizhangyang@hotmail.com>
2018-02-15 10:41:01 +0000

    add captcha

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
+$api = app('Dingo\Api\Routing\Router');
+
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
 ], function($api) {
@@ -28,5 +30,9 @@ $api->version('v1', [
         // 用户注册
         $api->post('users', 'UsersController@store')
             ->name('api.users.store');
+        //图片验证码
+        $api->post('captchas', 'CaptchasController@store')
+            ->name('api.captchas.store');
+
     });
 });
