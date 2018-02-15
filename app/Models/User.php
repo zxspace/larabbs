1238a91ad2c4d6c396054931ef7d30833d3ed710
zhangyang <whoshizhangyang@hotmail.com>
2018-02-15 19:08:24 +0000

    social authorizations

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
     }
 
     protected $fillable = [
-        'name', 'phone', 'email', 'password', 'introduction', 'avatar',
+        'name', 'phone', 'email', 'password', 'introduction', 'avatar', 'weixin_openid', 'weixin_unionid'
     ];
 
     public function replies()
