9d5b0849e863ad49eaa55108fc800e8161fc18ac
zhangyang <whoshizhangyang@hotmail.com>
2018-02-16 15:27:33 +0000

    topics show

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -10,6 +10,11 @@ use App\Models\User;
 
 class TopicsController extends Controller
 {
+    public function show(Topic $topic)
+    {
+        return $this->response->item($topic, new TopicTransformer());
+    }
+
     public function userIndex(User $user, Request $request)
     {
         $topics = $user->topics()->recent()
