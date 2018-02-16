70f8e70a549d8ed74191c2c00a751c9b637ff859
zhangyang <whoshizhangyang@hotmail.com>
2018-02-16 13:11:44 +0000

    user update

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\UserRequest;
 use App\Transformers\UserTransformer;
+use App\Models\Image;
 
 class UsersController extends Controller
 {
@@ -38,6 +39,22 @@ class UsersController extends Controller
             'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
         ])
         ->setStatusCode(201);
+    }
+
+    public function update(UserRequest $request)
+    {
+        $user = $this->user();
+
+        $attributes = $request->all();
+
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+
+            $attributes['avatar'] = $image->path;
+        }
+        $user->update($attributes);
+
+        return $this->response->item($user, new UserTransformer());
     }
 
     public function me()
